54134d5373b5e7ef3d51518227e294388b06e5c2
celiapaivab <celiadepaivabruno@gmail.com>
2025-01-11 11:46:15 -0300

    general styling done

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,17 +1,26 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
-import themeIcon from '../../assets/sun.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
+import sun from '../../assets/sun.svg'
+import moon from '../../assets/moon.svg'
+import githubLight from '../../assets/github-light.svg'
+import githubDark from '../../assets/github-dark.svg'
+import linkedinLight from '../../assets/linkedin-light.svg'
+import linkedinDark from '../../assets/linkedin-dark.svg'
 import CV from '../../assets/CeliaBrunoResume.pdf'
-
+import {useTheme} from '../../common/ThemeContext'
 
 function Hero() {
+    const {theme, toggleTheme} = useTheme();
+
+    const themeIcon = theme === 'light' ? sun : moon;
+    const githubIcon = theme === 'light' ? githubLight : githubDark;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
     return (
         <section id='hero' className={styles.container}>
             <div className={styles.colorModeContainer}>
                 <img className={styles.hero} src={heroImg} alt='Profile picture of Celia Bruno'></img>
-                <img className={styles.colorMode} src={themeIcon} alt='Color mode icon'></img>
+                <img className={styles.colorMode} src={themeIcon} alt='Color mode icon' onClick={toggleTheme}></img>
             </div>
             <div className={styles.info}>
                 <h1>Celia<br />Bruno</h1>
