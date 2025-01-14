138ca626ad94c34c5baec87339657b52549549a1
celiapaivab <celiadepaivabruno@gmail.com>
2025-01-14 10:26:59 -0300

    Contact session complete

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import styles from "./ContactStyles.module.css";
+
+function Contact() {
+    return (
+        <section id="contact" className={styles.container}>
+            <h1 className="sectionTittle">Contact</h1>
+            <form action="">
+                <div className="formGroup">
+                    <label htmlFor="name" hidden>
+                        Name
+                    </label>
+                    <input type="text" name="name" id="name" placeholder="Name" required></input>
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="email" hidden>
+                        Name
+                    </label>
+                    <input type="text" name="email" id="email" placeholder="Email" required></input>
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="message" hidden>
+                        Name
+                    </label>
+                    <textarea name="message" id="message" placeholder="Message" required></textarea>
+                </div>
+                <input className="hover btn" type="submit" value="Submit"></input>
+            </form>
+        </section>
+    );
+}
+
+export default Contact;
