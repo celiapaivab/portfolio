371d456f18b10ed9fa6e45bc2f7419dbf9b4f3fe
celiapaivab <celiadepaivabruno@gmail.com>
2025-01-14 12:11:19 -0300

    Adding send email to contact form

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,26 +4,47 @@ function Contact() {
     return (
         <section id="contact" className={styles.container}>
             <h1 className="sectionTittle">Contact</h1>
-            <form action="">
+            <form action="https://formspree.io/f/xpwwnaql" method="post">
                 <div className="formGroup">
                     <label htmlFor="name" hidden>
                         Name
                     </label>
-                    <input type="text" name="name" id="name" placeholder="Name" required></input>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Name"
+                        required
+                    ></input>
                 </div>
                 <div className="formGroup">
                     <label htmlFor="email" hidden>
                         Name
                     </label>
-                    <input type="text" name="email" id="email" placeholder="Email" required></input>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        required
+                    ></input>
                 </div>
                 <div className="formGroup">
                     <label htmlFor="message" hidden>
                         Name
                     </label>
-                    <textarea name="message" id="message" placeholder="Message" required></textarea>
+                    <textarea
+                        name="message"
+                        id="message"
+                        placeholder="Message"
+                        required
+                    ></textarea>
                 </div>
-                <input className="hover btn" type="submit" value="Submit"></input>
+                <input
+                    className="hover btn"
+                    type="submit"
+                    value="Submit"
+                ></input>
             </form>
         </section>
     );
